e70233aa5a09c3daee97413d07d6e09940cb0820
Christian S Barbosa <djchristiandf@gmail.com>
2023-03-14 00:43:10 -0300

    Abstração e encapsulamento
    Curso e Mentoria foram criadas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso cursoA = new Curso();
+        cursoA.setTitulo("Curso A");
+        cursoA.setDescricao("Descricao do curso A");
+        cursoA.setCargaHoraria(12);
+        Curso cursoB = new Curso();
+        cursoA.setTitulo("Curso B");
+        cursoA.setDescricao("Descricao do curso B");
+        cursoA.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria A");
+        mentoria.setDescricao("Grupo mentoria A");
+        mentoria.setData(LocalDate.now());
+        mentoria.setTitulo("Mentoria B");
+        mentoria.setDescricao("Grupo mentoria B");
+        mentoria.setData(LocalDate.now().plusMonths(1));
+
+        System.out.println(cursoA);
+        System.out.println(cursoB);
     }
 }
